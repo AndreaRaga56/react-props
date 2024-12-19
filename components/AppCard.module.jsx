1063bdfed406616e7fca9799a8f679e1d51828e8
AndreaRaga56 <andrearaga56@gmail.com>
2024-12-19 23:48:00 +0100

    end

--- components/AppCard.module.jsx
+++ components/AppCard.module.jsx
@@ -1,24 +1,20 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
 import style from "./AppCard.module.css";
 
-// eslint-disable-next-line react/prop-types
 function AppCard({ id, title, image, content, tags }) {
 
     let tagsElaborati = tags.map((curTag, i) => {
-
         return <span key={i} className={curTag}>{curTag} </span>
     })
 
     console.log(style.html)
-
-
-
 
     return (
         <div className={style.card}>
             {image ? <div> <img className={style.immagine} src={image} alt="" /></div> : <div> <img className={style.immagine} src="https://placehold.co/600x400" alt="" /></div>}
             <h3 className={style.titoloPost}>{title}</h3>
             <p className={style.paragrafo}>{content}</p>
-            <p className={style.paragrafo}></p>
             <div className={style.tags}>{tagsElaborati}</div>
             <div className={style.button}>LEGGI DI PIÙ</div>
         </div>
